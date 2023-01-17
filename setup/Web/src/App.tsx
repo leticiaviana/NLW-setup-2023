e4adbb987a968f356b8689d763588116a921d8ee
leticia_paixao <leticia.paixao@informata.com.br>
2023-01-17 15:07:54 -0300

    Inicia projetos

---
+++ setup/Web/src/App.tsx
@@ -0,0 +1,18 @@
+
+import { Habit } from './components/Habit'
+
+function App() {
+
+  return (
+    <div>
+    <Habit completed={3}/>
+    <Habit completed={10}/>
+    <Habit completed={20}/>
+    <Habit completed={50}/>
+    <Habit completed={55}/>
+    </div>
+    
+    )
+}
+
+export default App
